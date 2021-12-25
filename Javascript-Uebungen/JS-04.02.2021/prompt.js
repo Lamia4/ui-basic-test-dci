4e24b3e1fe9f2a3fe4d56a495d5bee2e9522cb13
Lamia <68332300+Lamia4@users.noreply.github.com>
2021-12-25 21:23:33 +0100

    Delete Javascript-Uebungen directory

--- Javascript-Uebungen/JS-04.02.2021/prompt.js
+++
@@ -1,3 +0,0 @@
-const prompt = require('prompt-sync')();
-const num1 = prompt ("Bitte Nummer eingeben:");
-console.log("Du hast " + num1 + " eingegeben.");
--- Javascript-Uebungen/t_pb03_functions_parametrics-RinaKazanovska-master/twofer.js
+++
@@ -1,8 +0,0 @@
-// Complete the function below so that it returns "Two for me and one for you" when no arguments are passed
-
-const twofer = who => {
-  return `Two for me and one for ${who}`;
-};
-
-console.log(twofer("Fran")); // -> "Two for me and one for Fran"
-console.log(twofer()); // -> "Two for me and one for you"
